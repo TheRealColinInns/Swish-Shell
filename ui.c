181e83bbadc268d4c505c1f09bb3c95f631f0f0a
TheRealColinInns <colin.inns@me.com>
2024-04-03 13:19:22 -0700

    User Interface Update

--- ui.c
+++ ui.c
@@ -51,6 +51,8 @@ static int tab_max;
 
 static char *built_list[4] = {"exit", "jobs", "history", "cd"};
 
+
+// initialize the user interface
 void init_ui(void)
 {
 
@@ -78,7 +80,6 @@ void init_ui(void)
 
 void set_status(int input_status){
     glob_status = input_status;
-    //LOG("New status: %d\n", glob_status);
 }
 
 void set_arrowing(void)
@@ -86,6 +87,7 @@ void set_arrowing(void)
     arrowing = false;
 }
 
+//free the memory allocated for the user interface
 void destroy_ui(void)
 {
     for(int i = 0; i<tab_max; i++){
@@ -94,6 +96,7 @@ void destroy_ui(void)
     free(tab_completions);
 }
 
+//display the location and status of the shell
 char *prompt_line(void)
 {
     const char *status = prompt_status() ? bad_str : good_str;
@@ -144,6 +147,7 @@ char *prompt_hostname(void)
     
 }
 
+//get the current working directory
 char *prompt_cwd(void)
 {
     char *cwd = getcwd(NULL, 0);
@@ -177,12 +181,11 @@ unsigned int prompt_cmd_num(void)
     return hist_last_cnum();
 }
 
+//read the inputed command
 char *read_command(void)
 {
-    
-
     if(scripting == true) {
-        char *line = NULL; //TODO memmory leak here
+        char *line = NULL;
         size_t line_sz = 0;
         ssize_t read_sz = getline(&line, &line_sz, stdin);
         line[read_sz - 1] = '\0';
@@ -211,6 +214,7 @@ int readline_init(void)
     return 0;
 }
 
+//navigate up with keyboard
 int key_up(int count, int key)
 {
     if(down){
@@ -278,13 +282,10 @@ int key_up(int count, int key)
     /* Move the cursor to the end of the line: */
     rl_point = rl_end;
 
-    // TODO: step back through the history until no more history entries are
-    // left. Once the end of the history is reached, stop updating the command
-    // line.
-
     return 0;
 }
 
+//navigate down with keyboard
 int key_down(int count, int key)
 {
     if(up) {
@@ -352,10 +353,6 @@ int key_down(int count, int key)
     /* Move the cursor to the end of the line: */
     rl_point = rl_end;
 
-    // TODO: step forward through the history (assuming we have stepped back
-    // previously). Going past the most recent history command blanks out the
-    // command line to allow the user to type a new command.
-
     return 0;
 }
 
@@ -375,13 +372,6 @@ char **command_completion(const char *text, int start, int end)
  */
 char *command_generator(const char *text, int state)
 {
-    
-    // TODO: find potential matching completions for 'text.' If you need to
-    // initialize any data structures, state will be set to '0' the first time
-    // this function is called. You will likely need to maintain static/global
-    // variables to track where you are in the search so that you don't start
-    // over from the beginning.
-
     LOG("STATE: %d\n", state);
 
     if (state==0){
@@ -393,11 +383,6 @@ char *command_generator(const char *text, int state)
 
         char *path = strdup(getenv("PATH")); //strdup
 
-        //if(path==NULL){
-            //path = getcwd(NULL, 0);
-        //}
-
-        
         LOG("text: %s\n", text);
 
         char **args = calloc(10, sizeof(char *));
@@ -425,9 +410,7 @@ char *command_generator(const char *text, int state)
         for (int i = 0; i<arg_size; i++) {
 
             char *loc_path = args[i];
-
             LOG("path: %s\n", args[i]);
-            
             DIR *directory_stream = opendir(strdup(args[i]));
 
             if (directory_stream == NULL) {
@@ -438,39 +421,29 @@ char *command_generator(const char *text, int state)
                 }
             }
 
-
-
             struct dirent* entry = NULL;
-
 
             while ((entry = readdir(directory_stream)) != NULL) {
 
                 if(entry->d_name[0]!='.') {
-                    //LOG("File: %s\n", entry->d_name);
                     struct stat stats;
-                    //LOGP("Attempting malloc...\n");
                     char *combined_path = malloc(sizeof(char) * (strlen(loc_path) + strlen(entry->d_name) + 1));
                     strcpy(combined_path, loc_path);
                     strcat(combined_path, "/");
                     strcat(combined_path, entry->d_name);
-                    //LOGP("Malloc Success\n");
-                    //LOG("Tot: %s\n", combined_path);
                     if(stat(combined_path, &stats)==-1) {
                         free(combined_path);
                         perror("stat");
                     } else {
                         free(combined_path);
                         if(stats.st_mode==33261){
-                            //LOG("Compare: %s | %s - %lu\n", entry->d_name, text, strlen(text));
                             if(strncmp(entry->d_name, text, strlen(text))==0){
                                 if (tab_loc<tab_max) {
                                     LOG("Addding: %s\n", entry->d_name);
                                     tab_completions[tab_loc] = strdup(entry->d_name);
-                                    //LOGP("Added\n");
                                     tab_loc++;
                                 } else {
                                     tab_max*=2;
-                                    //LOGP("Attempting Realloc...\n");
                                     char **tab_completions_temp = realloc(tab_completions, sizeof(char *)*tab_max);
                                     if(tab_completions_temp==NULL){
                                         free(tab_completions);
@@ -481,7 +454,6 @@ char *command_generator(const char *text, int state)
                                     }
                                     LOG("Addding: %s\n", entry->d_name);
                                     tab_completions[tab_loc] = strdup(entry->d_name);
-                                    //LOGP("Realloc Complete\n");
                                 }
                             }
                         }
@@ -516,7 +488,6 @@ char *command_generator(const char *text, int state)
     }
 
 builtins:
-    //LOG("Made it to builtins: %s\n", text);
     for(int i = built_loc; i<4; i++){
         if(text[0]=='\0'){
             built_loc++;
